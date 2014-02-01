d71678511e51febf55ef174a0431ca8dcc030402
Rachel Foecking <captainfoecking@gmail.com>
2014-02-01 13:03:25 -0500

    Script db name

--- scripts/sendcat.sh
+++ scripts/sendcat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sqlite3 /home/rachel/catpic/db/development.sqlite3 "SELECT * FROM users" | while read ROW; do
+sqlite3 $1 "SELECT * FROM users" | while read ROW; do
     id=`echo $ROW | awk '{split($0,a,"|"); print a[1]}'`
     name=`echo $ROW | awk '{split($0,a,"|"); print a[2]}'`
     email=`echo $ROW | awk '{split($0,a,"|"); print a[3]}'`
